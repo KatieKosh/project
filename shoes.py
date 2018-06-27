083dfdc7eb3a1ec834499b7400effc735aba036e
Kathryn <kkoshinski@hotmail.com>
2018-06-27 00:49:40 +0000

    changes made

--- shoes.py
+++ shoes.py
@@ -1,19 +1,29 @@
 from bs4 import BeautifulSoup
-from html.parser import HTMLParser
+# from html.parser import HTMLParser
 import requests
+from lxml import html
+
 
 url = "https://www.runnersworld.com/gear/a20853527/the-best-running-shoes-of-the-year/"
 r = requests.get(url)
 
-# print(text[0:3000])
+# print(text[0:2000])
 
 soup = BeautifulSoup(r.text, "html.parser")
 
-article = soup.find_all('p', class_='body-text')
+article = soup.find_all('h2', class_='body-h3', text=True)
+for text in article:
+    print(text)
+#article.find_all('p')
 
 # print(article)
+for h2_tag in soup.find_all('h2'):
+    print (h2_tag.text)
+    
 
 
+for p_tag in soup.find_all('strong'):
+    print (p_tag.text)
 
 # headline = soup.p
 print(article)
